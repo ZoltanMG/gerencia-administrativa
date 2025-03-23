548a40dbdee4047e68c5046173c0f5f52037d1e0
Zoltán MG <YX758pO0+A$%*Zd0m6)Gi>
2025-03-23 09:32:35 -0500

    se realiza prueba inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSG7P1tkuzB5lwh3ea0M7k3XQlcHLMSvOKNIODmWBQ_0s3xzzEWSnnpND467kse-EUSXkuS3JvFKZXQ/pub?output=csv";
+// const SHEET_URL_HOJA_2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSG7P1tkuzB5lwh3ea0M7k3XQlcHLMSvOKNIODmWBQ_0s3xzzEWSnnpND467kse-EUSXkuS3JvFKZXQ/pub?gid=94325495&output=csv";
+
+const fetchSheetData = async (url) => {
+  const response = await fetch(url);
+  const csvText = await response.text();
+  let rows = [];
+  csvText.split("\n").forEach((row, index) => {
+    const rowsSplit = row.split(",")
+    if (index !== 0) {
+      rows.push({
+        category: rowsSplit[0],
+        gid: rowsSplit[1]
+      })
+    }
+  });
+  return rows;
+};
 
 function App() {
+  const [main, setMain] = useState([])
+  useEffect(() => {
+    fetchSheetData(SHEET_URL).then((data) => {
+      console.log(data);
+      setMain(data)
+    }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gerencia Administrativa</h1>
+      {main.map((item) => (
+          <div key={item.gid}>
+            <button>{item.category}</button>
+          </div>
+        ))
+      }
     </div>
   );
 }
